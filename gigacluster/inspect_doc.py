2217418cc20e851373b338044ba6c7fe1b981c81
Joel Nothman <joel.nothman@gmail.com>
2014-04-23 14:35:11 +1000

    Faster inspect_doc by indexing docrep byte offsets

--- gigacluster/inspect_doc.py
+++ gigacluster/inspect_doc.py
@@ -14,6 +14,23 @@ def dr_to_string(doc):
         lines.append(' '.join(t.raw for t in doc.tokens[s.span]))
     return '\n'.join(lines) + '\n\n'
 
+
+def read_offset(path, key):
+    """Makes random access faster by seeking to appropriate offset
+
+    Offset indices (*.droffsets) are generated with:
+        for f in $(find /data1/gigacluster/streams/ -name "*.dr")
+        do
+            paste <(dr format py "doc.id.strip(\"'b\")" < $f) <(dr offsets < $f) > ${f}offsets
+        done
+    """
+    key += '\t'
+    with open(path) as f:
+        for l in f:
+            if l.startswith(key):
+                return int(l[len(key):])
+
+
 base = sys.argv[1]
 paths = {}
 for doc_id in sys.argv[2:]:
@@ -24,11 +41,19 @@ for doc_id in sys.argv[2:]:
 
 for path, doc_ids in sorted(paths.items()):
     with open(path, 'rb') as f:
-        for doc in dr.Reader(f, Doc):
-            # FIXME Lame!
-            doc_id = doc.id.lstrip("b'").rstrip("'")
-            if doc_id in doc_ids:
-                print(dr_to_string(doc))
-                doc_ids.remove(doc_id)
-                if not doc_ids:
-                    break
+        if os.path.exists(path + 'offsets'):
+            for doc_id in doc_ids:
+                offset = read_offset(path + 'offsets', doc_id)
+                if offset is None:
+                    raise "Id {!r} not found in offset index".format(doc_id)
+                f.seek(offset)
+                print(dr_to_string(next(dr.Reader(f, Doc))))
+        else:
+            for doc in dr.Reader(f, Doc):
+                # FIXME Lame!
+                doc_id = doc.id.lstrip("b'").rstrip("'")
+                if doc_id in doc_ids:
+                    print(dr_to_string(doc))
+                    doc_ids.remove(doc_id)
+                    if not doc_ids:
+                        break
